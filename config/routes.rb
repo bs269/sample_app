03a90d99a5778fad8efe70df17b287ef7c347d60
Ben Stephenson <ben@bluecircleweb.com>
2011-04-24 15:34:00 +0100

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 SampleApp::Application.routes.draw do  
+  get "users/new"
+
   match '/contact', :to => 'pages#contact'
   
   match '/about', :to => 'pages#about'
   
   match '/help', :to => 'pages#help'
+  
+  match '/signin', :to => 'pages#signin'
+  
+  match '/signup' :to => 'users#new'
   
   root :to => 'pages#home'
   
